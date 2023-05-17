30fff763f9eff5e0c82b0f4096d0efec63a74d70
shoaibkhan188626 <82094195+shoaibkhan188626@users.noreply.github.com>
2023-05-18 04:03:22 +0530

    first commit

---
+++ src/components/socialicons/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./style.css";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { socialprofils } from "../../content_option";
+
+export const Socialicons = (params) => {
+  return (
+    <div className="stick_follow_icon">
+      <ul>
+        {socialprofils.instagram && (
+          <li>
+            <a href={socialprofils.instagram}>
+              <FaInstagram />
+            </a>
+          </li>
+        )}
+
+        {socialprofils.github && (
+          <li>
+            <a href={socialprofils.github}>
+              <FaGithub />
+            </a>
+          </li>
+        )}
+
+        {socialprofils.linkedin && (
+          <li>
+            <a href={socialprofils.linkedin}>
+              <FaLinkedin />
+            </a>
+          </li>
+        )}
+      </ul>
+      <p>Connect Me</p>
+    </div>
+  );
+};
---
+++ src/content_option.js
@@ -0,0 +1,189 @@
+const logotext = "SHOAIB KHAN";
+const meta = {
+  title: "SHOAIB KHAN",
+  description:
+    "I’m SHOAIB KHAN a MERN stack devloper,currently working at Tekisky Nanded, Maharashtra",
+};
+
+const introdata = {
+  title: "I’m SHOAIB KHAN",
+  animated: {
+    first: "I EXCEL IN MERN STACK DEVELOPMENT",
+    second: "I CREATE DYNAMIC WEB APPLICATIONS",
+    third: "I BUILD ROBUST APIs AND BACKEND SYSTEMS",
+    fourth: "I BRING IDEAS TO LIFE WITH REACT AND NODE.JS",
+  },
+  description:
+    "I am SHOAIB KHAN, a results-driven MERN Stack Developer with a keen eye for detail and a strong focus on delivering clean and efficient code. With a passion for problem-solving and a deep understanding of the MERN stack technologies, I am dedicated to creating scalable and impactful web applications",
+  your_img_url:
+    "https://images.unsplash.com/photo-1517774076143-eece9f48df5f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=460&q=80",
+};
+
+const dataabout = {
+  title: "A bit about my self",
+  aboutme:
+    "Experienced MERN stack developer with one year of hands-on expertise in building robust and scalable web applications. Proficient in front-end technologies such as React, HTML, CSS, and JavaScript, along with back-end technologies including Node.js, Express, and MongoDB. Strong problem-solving skills and a passion for delivering high-quality code to meet client needs.",
+};
+const worktimeline = [
+  {
+    jobtitle: "FULL STACK DEVELOPER",
+    where: "TEKISKY PVT LTD",
+    date: "2022 - PRESENT",
+  },
+];
+
+const skills = [
+  {
+    name: "HTML & CSS",
+    value: 100,
+  },
+  {
+    name: "JAVASCRIPT",
+    value: 100,
+  },
+  {
+    name: "REACT JS",
+    value: 100,
+  },
+  {
+    name: "NEXT JS",
+    value: 100,
+  },
+  {
+    name: "SQL",
+    value: 100,
+  },
+  {
+    name: "DOCKER",
+    value: 100,
+  },
+  {
+    name: "LINUX",
+    value: 100,
+  },
+  {
+    name: "GIT",
+    value: 100,
+  },
+  {
+    name: "GSAP",
+    value: 100,
+  },
+  {
+    name: "NODE JS",
+    value: 100,
+  },
+  {
+    name: "MONGO DB",
+    value: 100,
+  },
+  {
+    name: "EXPRESS JS",
+    value: 100,
+  },
+  {
+    name: "CYBERSECURITY",
+    value: 100,
+  },
+  {
+    name: "VIRTUAL MACHINES",
+    value: 100,
+  },
+  {
+    name: "FIREBASE",
+    value: 100,
+  },
+  {
+    name: "MONGOOSE",
+    value: 100,
+  },
+  {
+    name: "THREE JS",
+    value: 100,
+  },
+];
+
+const services = [
+  {
+    title: "FRONT-END WEB DEVELOPMENT",
+    description:
+      "Experienced front-end web developer with one year of hands-on experience in React, HTML, CSS, JavaScript, Bootstrap, Tailwind CSS, Next.js, Three.js, GSAP, and WebGL.",
+  },
+  {
+    title: "BACK-END WEB DEVELOPMENT",
+    description:
+      "Experienced backend web developer with one year of hands-on experience in Node.js, Express, MongoDB (Mongoose), SQL.",
+  },
+  {
+    title: "DATABASE MANAGEMENT",
+    description:
+      "Experienced database management professional with one year of hands-on experience in MongoDB, including MongoDB Compass and Atlas.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://images.unsplash.com/photo-1617450365226-9bf28c04e130?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+    description: "The Mujtaba Helping Foundation",
+    link: "https://mhfglobal.com/",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1680084521631-e4e6d77704d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+    description:
+      "I developed and launched the official website for NSAKCET, providing a centralized platform for students and faculty to access important information and stay connected with the college community.",
+    link: "https://www.nsakcet.ac.in/",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1661956602944-249bcd04b63f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+    description:
+      "AN E-COMMERCE MADE USING REACT FOR FRONT-END AND NODE AND EXPRESS FOR BACK-END",
+    link: "#",
+  },
+  {
+    img: "https://360view.hum3d.com/zoom/Tools/T-Shirt_1000_0001.jpg",
+    description:
+      "A 3D T SHIRT CUSTOMIZING SITE MADE USING REACT AND DALLE-2 APIs.",
+    link: "#",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1607398202930-f4c68d729323?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80",
+    description:
+      "I developed a location-based React application that detects the user's location, displays nearby mosques within a 5 km radius, and provides accurate prayer times for enhanced convenience and accessibility.",
+
+    link: "#",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1492684223066-81342ee5ff30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    description:
+      "I developed a Next.js-powered event booking site, enabling users to browse, book, and attend various events with ease, providing a seamless experience for event organizers and attendees alike.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "shoaibullakhan15@gmail.com",
+  YOUR_FONE: "+91-8830488649",
+  description:
+    "I am a passionate MERN Stack Developer, driven by a love for creating innovative and user-centric web applications. With a strong attention to detail and a commitment to delivering high-quality solutions, I am dedicated to exceeding expectations and making a positive impact through my work. Let's collaborate and bring your ideas to life! ",
+  YOUR_SERVICE_ID: "service_xncjl97",
+  YOUR_TEMPLATE_ID: "template_ybkxkwp",
+  YOUR_USER_ID: "7nahqCVrmzfbLddHM",
+};
+
+const socialprofils = {
+  github: "https://github.com/shoaibkhan188626",
+  linkedin: "https://www.linkedin.com/in/shoaib-khan-522b48254/",
+  instagram: "https://instagram.com/shoebism?igshid=OTk0YzhjMDVlZA==",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
